fcb6957544748049142b1b60e61c64e29b92e985
ditaa99 <dita_s@live.com>
2025-11-04 22:19:11 +0100

    final changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import profile from "../assets/dita.jpg";
-import cv from "../assets/Dita_CV.pdf";
+import cv from "../assets/Dita_CV_short.pdf";
 
 const FaDownload = () => (
   <svg
